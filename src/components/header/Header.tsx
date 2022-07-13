fef9581140e965a912d88e9a7c9bc5006f8e21ca
Danilo <danilosdinizz@gmail.com>
2022-07-12 22:19:48 -0300

    STYLE: adding Tailwind CSS

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 export function Header(){
     return (
-        <header>
-            <h1>
+        <header >
+            <h1 className="bg-orange-500 w-full">
             header
             </h1>
         </header>

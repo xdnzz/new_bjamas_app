51039178662cfa83937977b1018d90959caac751
Danilo <danilosdinizz@gmail.com>
2022-07-16 01:23:12 -0300

    CHORE:add to cart button being created

---
+++ src/components/buyButton/BuyButton.tsx
@@ -0,0 +1,3 @@
+export function BuyButton(){
+    return <button className="bg-black text-white w-full">ADD TO CART</button>
+}
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,6 @@
 import Logo from '../../assets/logo.svg';
 import Cart from '../../assets/cart.svg';
+import { BuyButton } from '../buyButton/BuyButton';
 
 export function Header(){
     return (
@@ -7,6 +8,7 @@ export function Header(){
         <header className="py-9 flex justify-between">
             <img src={Logo} alt="Logo Bejamas"  className="w-28"/>
             <img src={Cart} alt="Cart Items" className="w-7" />
+            
         </header>
         <div className="bg-slate-300 w-full h-0.5"></div>
        </>
